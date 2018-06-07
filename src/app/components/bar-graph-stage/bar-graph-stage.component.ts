35f60224471fb6dbaf5607724459c7feafdc353f
KC Hunter <khunter@connectionseducation.com>
2018-06-07 12:08:53 -0400

    start using different method for d3 inside Angular 6.

--- src/app/components/bar-graph-stage/bar-graph-stage.component.ts
+++ src/app/components/bar-graph-stage/bar-graph-stage.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterContentInit } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewChild, ElementRef, Input } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -6,82 +6,25 @@ import * as d3 from 'd3';
   templateUrl: './bar-graph-stage.component.html',
   styleUrls: ['./bar-graph-stage.component.css']
 })
-export class BarGraphStageComponent implements AfterContentInit {
+export class BarGraphStageComponent implements OnInit {
+  @ViewChild('containerD3') d3Container: ElementRef;
 
-  constructor() { }
-
-  radius:   number;
-  data:any  = [];
-  margin:any  = [];
-  width: any;
-  height: any;
-  xScale: any;
-  yScale: any;
-  xAxis: any;
-  yAxis: any;
-  svgContainer: any;
-
-  public radius      = 10;
-
-  public data  = [
-    {'color': 'mixed', 'number': 9},
-    {'color': 'white', 'number': 2},
-    {'color': 'black', 'number': 3},
-    {'color': 'other', 'number': 6}
-  ];
+  constructor(
+    private elRef: ElementRef
+  ) { }
 
 
 
-  margin  = {top: 10, right: 10, bottom: 90, left: 10}
-  width   = 960 - this.margin.left - this.margin.right;
-  height  = 500 - this.margin.top - this.margin.bottom;
-  xScale  = d3.scaleBand().rangeRound([0, this.width]).padding(0.03);
-  yScale  = d3.scaleLinear().range([this.height, 0]);
-  xAxis   = d3.axisBottom(this.xScale);
-  yAxis   = d3.axisLeft(this.yScale);
+
+  ngOnInit(){}
+  //ngOnChange (){}
 
 
-  ngAfterContentInit(){
-
-    d3.select('p').style('color', 'red');
-
-
-    this.svgContainer = d3.select('.mySvg').append('svg')
-      .attr('width', this.width + this.margin.left + this.margin.right)
-      .attr('height', this.height + this.margin.top + this.margin.bottom)
-      .append('g').attr('class', 'container')
-      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
-
-    this.xScale.domain(this.data.map(function(d) { return d.color; }));
-    this.yScale.domain([0, d3.max(this.data, function(d) { return d.number; })]);
-
-    console.log('this x scale');
-    console.log(this.xScale);
-
-    this.svgContainer.selectAll('.bar')
-      .data(this.data)
-      .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', function(d){ return this.xScale(d.color); })
-      .attr('width', this.xScale.rangeBand())
-      .attr('y', function(d){ return this.yScale(d.number);})
-      .attr('height', function (d){ return this.height - this.yScale(d.number);});
-  }
-
-  clicked(event: any) {
-    d3.select(event.target).append('circle')
-        .attr('cx', event.x)
-        .attr('cy', event.y)
-        .attr('r', () => {
-          return this.radius;
-        })
-        .attr('fill', 'red');
-  }
 
   /** references
    * https://github.com/ultrasonicsoft/d3-ng5-demo/tree/master/src/app
    * https://medium.com/@tomastrajan/how-to-build-responsive-layouts-with-bootstrap-4-and-angular-6-cfbb108d797b
+   * https://github.com/HoplaGeiss/hopla-pie-chart/blob/master/src/app/pie-chart/pie-chart.component.ts
    */
 
 }
